2156321e5cc6e99f51af829e4f0d4cde561e2759
tabu97 <110127047+tabu97@users.noreply.github.com>
2023-06-09 12:46:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import snowflake.connector
 streamlit.text(' You will be Cordially Invited to the Exchanges')
 streamlit.text('...as soon as you submit your Account Locator')
 streamlit.text('...using the Streamlit form LINKED HERE!')
