4086aad593a2ed775c541976467a3c179f21453e
tabu97 <110127047+tabu97@users.noreply.github.com>
2023-06-09 12:45:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
-streamlit.text(' You will be Cordially Invited to the Exchanges
-...as soon as you submit your Account Locator
-...using the Streamlit form LINKED HERE!')
+streamlit.text(' You will be Cordially Invited to the Exchanges')
+streamlit.text('...as soon as you submit your Account Locator')
+streamlit.text('...using the Streamlit form LINKED HERE!')
